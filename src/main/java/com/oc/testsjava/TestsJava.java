02a15a0546a6d328b3373c6797c181fed9c2d527
Hugo Machado <hugomontes@hotmail.fr>
2022-10-28 23:58:08 +0200

    Test 2

--- src/main/java/com/oc/testsjava/TestsJava.java
+++ src/main/java/com/oc/testsjava/TestsJava.java
@@ -3,8 +3,8 @@ package com.oc.testsjava;
 public class TestsJava {
 
 	public static void main(String[] args) {
-		int i = 3;
-		System.out.println("HelloWorld ! " + i);
+
+		System.out.println("HelloWorld ! ");
 
 	}
 
